dec41f5531082ec2ccc36d7e0981c10a5f4804d3
munizt <gabrielmuniz.6419@gmail.com>
2025-03-31 08:30:10 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,39 @@ import { useState } from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('Ronaldo')
+  
 
-  return (
-    <div>
-      <h3> Pizzaria 2B</h3>
-<input className="nome" 
+  const Formulario = (props) => {
+    const [nome, setNome] = useState('Ronaldo')
+    return (
+      <div>
+        <h3>{props.titulo}</h3>
+        <input className="nome" 
   placeholder="Digite seu nome..."
   onChange={(e)=>{setNome(e.target.value)}}
   name="nome"
   type="text" />
-<button className="botao" onClick={()=>alert(nome)}>
+<button className="botao" onClick={
+        () => {
+          if(nome.length> 0)
+            alert(nome)
+          else
+          {alert(props.mensagem)}>
+        }
+      }>
     CLIQUE AQUI
 </button>  
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3> Pizzaria 2B</h3>
+      <Formulario titulo = "Nome" mensagem = " curintia" />
+      <Formulario titulo = " E-mail"  mensagem = " campeao paulista"/>
+      <Formulario titulo = " CPF " mensagem = " bicampeao "/>
+      <Formulario/>
     </div>
   )
 }
