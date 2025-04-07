3ceb2eb21464cd310b94c15700c9e9086a846328
munizt <gabrielmuniz.6419@gmail.com>
2025-04-07 11:05:22 +0000

    alteração vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,49 @@
 import { useState } from "react"
-
-function App(){
-
-  
-
+ 
+function App() {
+ 
+ 
+ 
   const Formulario = (props) => {
+ 
     const [nome, setNome] = useState('Ronaldo')
+ 
     return (
       <div>
         <h3>{props.titulo}</h3>
-        <input className="nome" 
-  placeholder="Digite seu nome..."
-  onChange={(e)=>{setNome(e.target.value)}}
-  name="nome"
-  type="text" />
-<button className="botao" onClick={
-        () => {
-          if(nome.length> 0)
-            alert(nome)
-          else
-          {alert(props.mensagem)}>
-        }
-      }>
-    CLIQUE AQUI
-</button>  
+        <input className="nome"
+          placeholder="Digite seu nome..."
+          onChange={(e) => { setNome(e.target.value) }}
+          name="nome"
+          type="text" />
+        <button className="botao" onClick={
+                () => {
+                  if(nome.length > 0)
+                    alert(nome)
+                  else
+                    alert(props.mensagem)
+                }
+              }>
+          CLIQUE AQUI
+        </button>
       </div>
     )
   }
-
+ 
   return (
     <div>
       <h3> Pizzaria 2B</h3>
-      <Formulario titulo = "Nome" mensagem = " curintia" />
-      <Formulario titulo = " E-mail"  mensagem = " campeao paulista"/>
-      <Formulario titulo = " CPF " mensagem = " bicampeao "/>
-      <Formulario/>
+      <Formulario titulo = "Nome" mensagem = "Santos" />
+      <Formulario titulo = "E-mail" mensagem = "Curintia" />
+      <Formulario titulo = "CPF" mensagem = "Parmera" />
+      <Formulario />
+      <Formulario />
+      <Formulario />
+ 
     </div>
   )
 }
-
+ 
 export default App
-
+ 
+ 
